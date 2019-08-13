a9c98d6e0f2e58c5ff5aa1ca1377ac1a21d36a98
tumatihub <jgsoaresdias@gmail.com>
2019-08-13 20:57:49 -0300

    Change StrongEnemies material when hit

---
+++ Assets/Scripts/StrongEnemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrongEnemy : Enemy
+{
+    [SerializeField] Material _lowHealthMaterial;
+    MeshRenderer _mesh;
+
+    protected override void Start()
+    {
+        base.Start();
+        _mesh = transform.Find("EnemyBody").GetComponent<MeshRenderer>();
+    }
+
+    public override int TakeDamage(int amount, Vector3 impactDir)
+    {
+        var _hitPointsRemaining = base.TakeDamage(amount, impactDir);
+
+        if (_hitPointsRemaining == 1)
+        {
+            _mesh.material = _lowHealthMaterial;
+        }
+
+        return _hitPointsRemaining;
+    }
+}
