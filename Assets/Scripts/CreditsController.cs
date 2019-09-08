cd7707509ae1260fe82888e0e2b8738e95a7b1cb
tumatihub <jgsoaresdias@gmail.com>
2019-09-08 19:19:21 -0300

    Final credits

---
+++ Assets/Scripts/CreditsController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsController : MonoBehaviour
+{
+
+    [SerializeField] Weapon _wpn;
+
+    void Start()
+    {
+        StartCoroutine(Shoot());
+    }
+
+    IEnumerator Shoot()
+    {
+        _wpn.Activate();
+        yield return new WaitForSeconds(1f);
+        _wpn.Shoot();
+        yield return new WaitForSeconds(6f);
+        SceneManager.LoadScene(0);
+    }
+}
